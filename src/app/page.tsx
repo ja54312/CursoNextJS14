d93138890820de52a8b6908b5d35259b0fdabd74
JA54312 <ija54312@gmail.com>
2024-06-13 12:52:55 -0600

    rutas dinamicas y estaticas

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h2>Hola Mundo</h2>
+    </main>
+  );
+}
---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,14 @@
+interface CategoryProps {
+    params: {
+        category: string
+    }
+}
+export default function Category(props: CategoryProps) {
+    //console.log(props)
+    const { category } = props.params
+    //console.log(category)
+
+    return (
+        <h1>Categoria Dinamica:{category}</h1>
+    )
+}
