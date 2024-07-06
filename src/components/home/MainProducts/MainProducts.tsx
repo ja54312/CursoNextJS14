bebd3d1ca03f4cb5a283d7e310e1418ec7db0726
JA54312 <ija54312@gmail.com>
2024-07-06 11:19:57 -0600

    api

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,5 +1,16 @@
-export const MainProducts = () => {
-    //console.log(process.env.SHOPIFY_HOSTNAME)
+const getProducts = async () => {
+    const response = await fetch(`https://${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`, {
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY as string
+        })
+    })
+    const data = await response.json()
+    return data
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts()
+    console.log(products)
     return (
         <section>
             <h1>Main Products</h1>
