58a1df6e0a3d52076d1fcf3f2753462e15074a37
JA54312 <ija54312@gmail.com>
2024-07-30 12:56:18 -0600

    redirect

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,6 +1,8 @@
 
+import { redirect } from "next/navigation"
 import { ProductView } from "../../../components/product/ProductView"
 import { getProducts } from "../../../services/shopify/products"
+
 
 interface ProductPageProps {
     searchParams: {
@@ -12,6 +14,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
     const id = searchParams.id
     const products = await getProducts(id)
     const product = products[0]
+
+    if (!id) {
+        redirect('/')
+    }
 
     return <ProductView product={product} />
 }
