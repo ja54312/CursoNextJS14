ea3968827bcefc56b67fd45b8804bee74ed1ec08
JA54312 <ija54312@gmail.com>
2024-08-15 12:34:26 -0600

    hook zuztand

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { useState } from "react";
+import { useShoppingCart } from "../../../hook/useShoppingCart";
 import { FaShoppingCart } from "react-icons/fa";
 import styles from './ShoppingCart.module.sass'
 
 export const ShoppingCart = () => {
-    const [countItems, setCountItems] = useState(0);
+    const { cart } = useShoppingCart();
 
     return (
         <button className={styles.ShoppingCart}>
             <span className={styles.ShoppingCart__counter}>
-                {countItems}
+                {cart.length}
             </span>
             <FaShoppingCart />
         </button>
