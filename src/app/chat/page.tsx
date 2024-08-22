c0c84e6ef74bf1bdca5e0881ffdff858402befd3
JA54312 <ija54312@gmail.com>
2024-08-22 11:33:13 -0600

    chatbot

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import Chat from "../../components/chat/chat"
+
+export default function ChatPage() {
+    return (
+        <>
+            <h1>Chatbot</h1>
+            <Chat />
+        </>
+    )
+}
