a5372be905a46209f7e3406d836700e915c33607
JA54312 <ija54312@gmail.com>
2024-08-23 10:12:02 -0600

    create agent

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,18 @@
-import Chat from "../../components/chat/chat"
+import { Chat } from "../../components/chat/chat"
+import { getProducts } from "../../services/shopify/products"
+import { createAgent } from "../../utils/openai/createAgent"
 
-export default function ChatPage() {
+export default async function ChatPage() {
+
+    const products = await getProducts()
+    const productTitles = products.map((product) => product.title)
+    const flatProductTitles = productTitles.join("\n")
+    const agent = createAgent(flatProductTitles)
+
     return (
         <>
             <h1>Chatbot</h1>
-            <Chat />
+            <Chat agent={agent} />
         </>
     )
 }
--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,27 +1,44 @@
-'use client'
+'use client';
 
-import { useChat } from 'ai/react'
+import { useChat } from 'ai/react';
 
-export default function Chat() {
-    const { messages, input, handleInputChange, handleSubmit } = useChat()
+export const Chat = (props: { agent: string }) => {
+    const { messages, input, handleInputChange, handleSubmit } = useChat({
+        initialMessages: [
+            {
+                id: '1',
+                role: 'system',
+                content: props.agent
+            }
+        ],
+    });
 
     return (
-        <div>
-            {messages.map(m => (
-                <div key={m.id}>
-                    {m.role}: {m.content}
-                </div>
-            ))}
-
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Say something...
-                    <input
-                        value={input}
-                        onChange={handleInputChange}
-                    />
-                </label>
+        <main className="mx-auto w-full h-screen max-w-lg p-24 flex flex-col">
+            <section className="mb-auto m">
+                {messages
+                    .filter(m => m.role !== 'system')
+                    .map(m => (
+                        <div className="mb-4" key={m.id}>
+                            {m.role === 'user' ? 'User: ' : 'AI: '}
+                            {m.content}
+                        </div>
+                    ))}
+            </section>
+            <form className="flex space-x-4" onSubmit={handleSubmit}>
+                <input
+                    className="rounded-md p-2 text-black"
+                    value={input}
+                    onChange={handleInputChange}
+                    placeholder="Say something..."
+                />
+                <button
+                    className="border-solid border-2 border-white p-2 rounded-md"
+                    type="submit"
+                >
+                    Send
+                </button>
             </form>
-        </div>
-    )
+        </main>
+    );
 }
